9c2af3b3eaf0bde08126f711a40f641d79d4d6cc
anastasia <smolina.anastasia.14@gmail.com>
2021-02-23 21:10:08 +0300

    api

--- api.js
+++ api.js
@@ -1,15 +1,25 @@
 document.addEventListener("DOMContentLoaded", function(event) {
-    showAstroPhoto(0);
+    showMarsPhoto(0);
+    showAstroPhoto();
 });
 
-function showAstroPhoto(i) {
+function showMarsPhoto(i) {
     fetch('https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&camera=fhaz&api_key=KiFUeoHdAPdwvsR3Ldo5n7geSSbJCtTZcQtj8tLu')
     .then(response => response.json())
     .then(marsRover => {
         console.log(marsRover);
         document.getElementById('marsRover-name').innerHTML = (marsRover.photos[i].rover.name);
         document.getElementById('earth-date').innerHTML = (marsRover.photos[i].earth_date);
-        document.getElementById('astro-ph').src = (marsRover.photos[i].img_src);
+        document.getElementById('mars-ph').src = (marsRover.photos[i].img_src);
+    })
+}
+
+function showAstroPhoto() {
+    fetch('https://api.nasa.gov/planetary/apod?api_key=KiFUeoHdAPdwvsR3Ldo5n7geSSbJCtTZcQtj8tLu')
+    .then(response => response.json())
+    .then(photo => {
+        console.log(photo);
+        document.getElementById('astro-ph').src = (photo.url);
     })
 }
 
